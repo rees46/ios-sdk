9b49e98028c763722f12d0cb1783ea648bbfc42f
Michael Kechinov <mkechinov@Michaels-MacBook-Pro.local>
2021-09-05 17:17:02 +0300

    1.5.10. Expand recommenders response

--- REES46/Classes/Model/RecommendResponse.swift
+++ REES46/Classes/Model/RecommendResponse.swift
@@ -24,32 +24,54 @@ public struct RecommenderResponse {
 
 public struct Recommended {
     public var id: String = ""
+    public var barcode: String = ""
     public var name: String = ""
     public var brand: String = ""
+    public var model: String = ""
+    public var description: String = ""
     public var imageUrl: String = ""
+    public var resizedImageUrl: String = ""
     public var url: String = ""
     public var categories = [Category]()
     public var price: Double = 0
     public var oldPrice: String?
     public var currency: String = ""
+    public var salesRate: Int = 0
+    public var discount: Bool = false
+    public var relativeSalesRate: Float = 0.0
+    public var paramsRaw: [[String: Any]]?
 
     init(json: [String: Any]) {
         id = json["id"] as? String ?? ""
+        barcode = json["barcode"] as? String ?? ""
         name = json["name"] as? String ?? ""
         brand = json["brand"] as? String ?? ""
+        model = json["model"] as? String ?? ""
+        description = json["description"] as? String ?? ""
         imageUrl = json["image_url"] as? String ?? ""
+        resizedImageUrl = json["picture"] as? String ?? ""
         url = json["url"] as? String ?? ""
+        price = json["price"] as? Double ?? 0
+        oldPrice = json["oldprice"] as? String
+        currency = json["currency"] as? String ?? ""
+        salesRate = json["sales_rate"] as? Int ?? 0
+        relativeSalesRate = json["relative_sales_rate"] as? Float ?? 0.0
+        discount = json["discount"] as? Bool ?? false
+        
         let cats = json["categories"] as? [[String: Any]] ?? []
         var catsTemp = [Category]()
         for item in cats {
             catsTemp.append(Category(json: item))
         }
         categories = catsTemp
-        price = json["price"] as? Double ?? 0
-        oldPrice = json["oldprice"] as? String
-        currency = json["currency"] as? String ?? ""
+        
+        // TODO: convert to objects
+        paramsRaw = json["params"] as? [[String: Any]]
+        
     }
 }
+
+
 
 
 public enum RecommendedByCase: String {
