2a524a8a428f1964e6564d3db3db42e4376e0446
pavel <pp@rees46.ru>
2023-05-10 15:59:57 +0300

    Stories.v3.3.4 upd.2

--- DemoApp/Pods/Target Support Files/Pods-REES46_Tests/Pods-REES46_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_REES46_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_REES46_Tests
-@end
--- DemoApp/Pods/Target Support Files/REES46/REES46-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_REES46 : NSObject
-@end
-@implementation PodsDummy_REES46
-@end
