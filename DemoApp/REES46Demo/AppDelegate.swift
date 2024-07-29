2ec0e9e62ae4a45102f1e60233395c001f365e89
Daniel Green <gdl@rees46.ru>
2024-07-29 20:54:25 +0200

    feat(notification): Add func for detecting delivered message

--- DemoApp/REES46Demo/AppDelegate.swift
+++ DemoApp/REES46Demo/AppDelegate.swift
@@ -75,11 +75,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func configureFirebase() {
         let ciEnv = ProcessInfo.processInfo.environment[Constants.actionsKey] ?? Constants.actionsNegativeValue
-        
+        print("ciEnv: \(ciEnv)")
+        print("actionsKey: \(Constants.actionsKey), actionsPositiveValue: \(Constants.actionsPositiveValue)")
+
         if ciEnv == Constants.actionsPositiveValue {
             print("Skipping Firebase configuration in CI")
         } else {
             FirebaseApp.configure()
+            print("Firebase configured successfully")
         }
     }
     
