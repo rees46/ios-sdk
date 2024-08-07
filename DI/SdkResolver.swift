6379d69f35a0bc02369172156b1ed4af29d4e593
Daniel Green <gdl@rees46.ru>
2024-08-07 12:30:00 +0200

    fix(sdk): Changed file paths in project for services and protocols

--- DI/SdkResolver.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import Resolver
-
-extension Resolver {
-    static func registerSdkPartsImpl() {
-        register { SearchServiceImpl(sdk: resolve()) as SearchService }.scope(.application)
-        register { PushTokenHandlerServiceImpl(sdk: resolve()) as PushTokenNotificationService }.scope(.application)
-    }
-}
