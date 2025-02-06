d947486ec013fef28eb2af121d8f6cba90193fbd
Nobu <omabrirdu@yandex.ru>
2025-02-07 00:22:16 +0500

    fix: impl-validation
    moved validators to regex folder

--- REES46/Classes/Extensions/EmailValidator.swift
+++ REES46/Classes/Extensions/EmailValidator.swift
@@ -7,12 +7,3 @@ func isValid(email:String) -> Bool {
   
   return emailPredicate.evaluate(with:email)
 }
-
-func isValid(phone:String) -> Bool {
-  
-  let phoneRegex = "^\\+?[0-9]{1,4}[\\s-]?\\(?[0-9]{3}\\)?[\\s-]?[0-9]{3}[\\s-]?[0-9]{2}[\\s-]?[0-9]{2}$"
-  let phonePredicate = NSPredicate(format: "SELF MATCHES %@", phoneRegex)
-  
-  return phonePredicate.evaluate(with: phone)
-}
-
---
+++ REES46/Classes/Extensions/PhoneValidator.swift
@@ -0,0 +1,8 @@
+func isValid(phone:String) -> Bool {
+  
+  let phoneRegex = "^\\+?[0-9]{1,4}[\\s-]?\\(?[0-9]{3}\\)?[\\s-]?[0-9]{3}[\\s-]?[0-9]{2}[\\s-]?[0-9]{2}$"
+  let phonePredicate = NSPredicate(format: "SELF MATCHES %@", phoneRegex)
+  
+  return phonePredicate.evaluate(with: phone)
+}
+
