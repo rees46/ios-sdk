44351aa418f6a29c4ea15b396df00e804a6c0a5a
Nobu <omabrirdu@yandex.ru>
2025-02-12 15:10:13 +0500

    refactor(chore): move log services to new file

--- REES46/Classes/Notifications/NotificationLogger.swift
+++ REES46/Classes/Notifications/NotificationLogger.swift
@@ -1,24 +1,23 @@
 public class NotificationLogger{
   private let logTag = "PUSH"
-  private let sdk: PersonalizationSDK
-  private let notificationService: NotificationService
-  
+  private var sdk: PersonalizationSDK?
+  private weak var notificationService: NotificationService?
+
   public init(sdk: PersonalizationSDK) {
     self.sdk = sdk
-    self.notificationService = NotificationService(sdk: sdk)
   }
   
   public func log(_ message: String) {
     print("📩\(logTag): \(message)")
   }
   public func notificationClicked(type: String, code: String) {
-    notificationService.sdk.notificationClicked(type: type, code: code) { error in
+    notificationService?.sdk.notificationClicked(type: type, code: code) { error in
       self.log("Notification Clicked: \(error)")
     }
   }
   
   public func notificationReceived(type: String, code: String) {
-    notificationService.sdk.notificationReceived(type: type, code: code) { error in
+    notificationService?.sdk.notificationReceived(type: type, code: code) { error in
       self.log("Notification Received: \(error)")
     }
   }
