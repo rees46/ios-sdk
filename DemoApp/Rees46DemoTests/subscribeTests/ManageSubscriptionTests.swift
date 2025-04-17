41d1866f5b233826e51375d8d885f6960a8d6e92
Arina Gazhina <gazhina@internet.ru>
2025-04-17 23:57:09 +0300

    feat(subscription): test manageSubscription delays

--- DemoApp/Rees46DemoTests/subscribeTests/ManageSubscriptionTests.swift
+++ DemoApp/Rees46DemoTests/subscribeTests/ManageSubscriptionTests.swift
@@ -5,7 +5,7 @@ class ManageSubscriptionTests: XCTestCase {
     
     private let testShopId = "357382bf66ac0ce2f1722677c59511"
     private let testItemId = "486"
-    private let testEmail = "managesubscription@mail.ru"
+    private let testEmail = "ga@rees46.ru"
     private let testPhone = "+79966999666"
     private let testCurrentPrice = 170.0
     private let testItemIds = ["486"]
@@ -28,72 +28,80 @@ class ManageSubscriptionTests: XCTestCase {
     func testManageSubscription_withEmailOnly() {
         let expectation = XCTestExpectation(description: "Subscribe with email only")
         
-        sdk.manageSubscription(
-            email: testEmail,
-            completion: { result in
-                switch result {
-                case .success:
-                    print("Subscribe with email succeeded")
-                case .failure(let error):
-                    XCTFail("Subscribe with email failed: \(error.localizedDescription)")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.sdk.manageSubscription(
+                email: self.testEmail,
+                completion: { result in
+                    switch result {
+                    case .success:
+                        print("Subscribe with email succeeded")
+                    case .failure(let error):
+                        XCTFail("Subscribe with email failed: \(error.localizedDescription)")
+                    }
+                    expectation.fulfill()
                 }
-                expectation.fulfill()
-            }
-        )
+            )
+        }
         wait(for: [expectation], timeout: 5.0)
     }
     
     func testManageSubscription_withPhoneOnly() {
         let expectation = XCTestExpectation(description: "Subscribe with phone only")
         
-        sdk.manageSubscription(
-            phone: testPhone,
-            completion: { result in
-                switch result {
-                case .success:
-                    print("Subscribe with phone succeeded")
-                case .failure(let error):
-                    XCTFail("Subscribe with phone failed: \(error.localizedDescription)")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.sdk.manageSubscription(
+                email: self.testEmail,
+                completion: { result in
+                    switch result {
+                    case .success:
+                        print("Subscribe with email succeeded")
+                    case .failure(let error):
+                        XCTFail("Subscribe with email failed: \(error.localizedDescription)")
+                    }
+                    expectation.fulfill()
                 }
-                expectation.fulfill()
-            }
-        )
+            )
+        }
         wait(for: [expectation], timeout: 5.0)
     }
     
     func testManageSubscription_withEmailAndPhone() {
         let expectation = XCTestExpectation(description: "Subscribe with email and phone")
         
-        sdk.manageSubscription(
-            email: testEmail,
-            phone: testPhone,
-            completion: { result in
-                switch result {
-                case .success:
-                    print("Subscribe with email and phone succeeded")
-                case .failure(let error):
-                    XCTFail("Subscribe with email and phone failed: \(error.localizedDescription)")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.sdk.manageSubscription(
+                email: self.testEmail,
+                completion: { result in
+                    switch result {
+                    case .success:
+                        print("Subscribe with email succeeded")
+                    case .failure(let error):
+                        XCTFail("Subscribe with email failed: \(error.localizedDescription)")
+                    }
+                    expectation.fulfill()
                 }
-                expectation.fulfill()
-            }
-        )
+            )
+        }
         wait(for: [expectation], timeout: 5.0)
     }
     
     func testManageSubscription_withoutContactInfo() {
         let expectation = XCTestExpectation(description: "Subscribe without contact info")
         
-        sdk.manageSubscription(
-            completion: { result in
-                switch result {
-                case .success:
-                    print("Subscribe with did succeeded")
-                case .failure(let error):
-                    XCTFail("Subscribe with did failed: \(error.localizedDescription)")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.sdk.manageSubscription(
+                email: self.testEmail,
+                completion: { result in
+                    switch result {
+                    case .success:
+                        print("Subscribe with email succeeded")
+                    case .failure(let error):
+                        XCTFail("Subscribe with email failed: \(error.localizedDescription)")
+                    }
+                    expectation.fulfill()
                 }
-                expectation.fulfill()
-            }
-        )
+            )
+        }
         wait(for: [expectation], timeout: 5.0)
     }
 }
