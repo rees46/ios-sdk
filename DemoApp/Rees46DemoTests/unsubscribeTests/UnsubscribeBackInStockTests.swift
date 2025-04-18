7aee5becd915338f8ecc491f3804093d0b26d046
Arina Gazhina <gazhina@internet.ru>
2025-04-18 21:08:09 +0300

    feat(subscription): use mock generator for phone/email for unsubscribe back in stock tests

--- DemoApp/Rees46DemoTests/unsubscribeTests/UnsubscribeBackInStockTests.swift
+++ DemoApp/Rees46DemoTests/unsubscribeTests/UnsubscribeBackInStockTests.swift
@@ -4,16 +4,18 @@ import XCTest
 class UnsubscribeBackInStockTests: XCTestCase {
     
     private let testShopId = "357382bf66ac0ce2f1722677c59511"
-    private let testItemId = "486"
-    private let testEmail = "ga@rees46.ru"
-    private let testPhone = "+79966999666"
-    private let testCurrentPrice = 170.0
     private let testItemIds = ["486"]
+    
+    private var testEmail: String!
+    private var testPhone: String!
     
     var sdk: PersonalizationSDK!
     
     override func setUp() {
         super.setUp()
+        testEmail = MockGenerator.generateEmail()
+        testPhone = MockGenerator.generatePhoneNumber()
+        
         sdk = createPersonalizationSDK(
             shopId: testShopId,
             apiDomain: "api.rees46.ru",
@@ -25,8 +27,7 @@ class UnsubscribeBackInStockTests: XCTestCase {
         sdk = nil
         super.tearDown()
     }
-    
-    
+
     func testUnsubscribeBackInStock_withEmailOnly() {
         let expectation = XCTestExpectation(description: "Unsubscribe with email only")
         
@@ -35,10 +36,10 @@ class UnsubscribeBackInStockTests: XCTestCase {
             email: testEmail,
             completion: { result in
                 switch result {
-                case .success:
-                    print("Unsubscribe with email succeeded")
-                case .failure(let error):
-                    XCTFail("Unsubscribe with email failed: \(error.localizedDescription)")
+                    case .success:
+                        print("Unsubscribe with email succeeded")
+                    case .failure(let error):
+                        XCTFail("Unsubscribe with email failed: \(error.localizedDescription)")
                 }
                 expectation.fulfill()
             }
@@ -54,17 +55,17 @@ class UnsubscribeBackInStockTests: XCTestCase {
             phone: testPhone,
             completion: { result in
                 switch result {
-                case .success:
-                    print("Unsubscribe with phone succeeded")
-                case .failure(let error):
-                    XCTFail("Unsubscribe with phone failed: \(error.localizedDescription)")
+                    case .success:
+                        print("Unsubscribe with phone succeeded")
+                    case .failure(let error):
+                        XCTFail("Unsubscribe with phone failed: \(error.localizedDescription)")
                 }
                 expectation.fulfill()
             }
         )
         wait(for: [expectation], timeout: 5.0)
     }
-    
+
     func testUnsubscribeBackInStock_withEmailAndPhone() {
         let expectation = XCTestExpectation(description: "Unsubscribe with email and phone")
         
@@ -74,10 +75,10 @@ class UnsubscribeBackInStockTests: XCTestCase {
             phone: testPhone,
             completion: { result in
                 switch result {
-                case .success:
-                    print("Unsubscribe with email and phone succeeded")
-                case .failure(let error):
-                    XCTFail("Unsubscribe with email and phone failed: \(error.localizedDescription)")
+                    case .success:
+                        print("Unsubscribe with email and phone succeeded")
+                    case .failure(let error):
+                        XCTFail("Unsubscribe with email and phone failed: \(error.localizedDescription)")
                 }
                 expectation.fulfill()
             }
@@ -92,10 +93,10 @@ class UnsubscribeBackInStockTests: XCTestCase {
             itemIds: testItemIds,
             completion: { result in
                 switch result {
-                case .success:
-                    print("Unubscribe with did succeeded")
-                case .failure(let error):
-                    XCTFail("Unubscribe with did failed: \(error.localizedDescription)")
+                    case .success:
+                        print("Unubscribe with did succeeded")
+                    case .failure(let error):
+                        XCTFail("Unubscribe with did failed: \(error.localizedDescription)")
                 }
                 expectation.fulfill()
             }
