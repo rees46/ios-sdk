eb411f79599757d2c240b7655918c410f2852f35
Arina Gazhina <gazhina@internet.ru>
2025-04-23 13:40:36 +0300

    chore(tests): return phone

--- DemoApp/Rees46DemoTests/MockGenerator.swift
+++ DemoApp/Rees46DemoTests/MockGenerator.swift
@@ -3,9 +3,10 @@ import Fakery
 class MockGenerator {
     private static let faker = Faker()
     
-    static func generatePhoneNumber(prefix: String = "+7", length: Int = 10) -> String {
+    static func generatePhoneNumber(prefix: String = Constants.phonePrefix, length: Int = Constants.phoneLength) -> String {
         let digits = (0..<length).map { _ in String(Int.random(in: 0...9)) }.joined()
-        return prefix + digits
+        let phone = prefix + digits
+        return phone
     }
     
     static func generateEmail() -> String {
