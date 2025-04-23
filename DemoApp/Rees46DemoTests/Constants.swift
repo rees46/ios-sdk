c59ba056cab5a287236fe77ea46ff7e79b6e96c4
Arina Gazhina <gazhina@internet.ru>
2025-04-23 13:35:29 +0300

    feat(tests): add constants

---
+++ DemoApp/Rees46DemoTests/Constants.swift
@@ -0,0 +1,9 @@
+struct Constants {
+    static let phonePrefix = "+7"
+    static let phoneLength = 10
+    static let testShopId = "357382bf66ac0ce2f1722677c59511"
+    static let testItemIds = ["486"]
+    static let testCurrentPrice = 170.0
+    static let testApiDomain = "api.rees46.ru"
+    static let testItemId = "486"
+}
